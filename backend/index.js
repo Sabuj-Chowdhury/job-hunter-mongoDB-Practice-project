4ed62ecaf7599714975d51444b30305f48057368
Sabuj-Chowdhury <csabuj73@gmail.com>
2024-12-14 13:50:54 +0530

    create get  API for all jobs from db

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,18 @@
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
+
+// very import to remember
+require("dotenv").config();
+
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
+
+// why am i getting the undefined value ???
+// console.log(process.env.DB_PASS);
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.i53p4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -18,6 +25,15 @@ const client = new MongoClient(uri, {
 });
 async function run() {
   try {
+    // Get the database and collection on which to run the operation
+    const jobsCollection = client.db("jobhunterDB").collection("jobs");
+
+    // creating API for all job
+    app.get("/jobs", async (req, res) => {
+      const result = await jobsCollection.find().toArray();
+      res.send(result);
+    });
+
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
