947f8f371d5034ff6e14f7f9b2f0be2c7857f3f5
Sabuj-Chowdhury <csabuj73@gmail.com>
2024-12-16 15:15:07 +0530

    Cookie send on the response from backend,Just learning about the cookie

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,9 @@
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-// very import to remember
-require("dotenv").config();
+
+require("dotenv").config(); // very import to remember
+
+const cookieParser = require("cookie-parser"); // to use cookie-parser
+
 const express = require("express");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
@@ -10,6 +13,8 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
+
+app.use(cookieParser()); // using cookie parser
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.i53p4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -32,8 +37,13 @@ async function run() {
     // jwt (json web token) related api
     app.post("/jwt", async (req, res) => {
       const user = req.body;
-      const token = jwt.sign(user, "secret", { expiresIn: "1h" });
-      res.send(token);
+      const token = jwt.sign(user, process.env.JWT_SECRET, { expiresIn: "1h" });
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: false, //set true for production in http //DEPLOY **********
+        })
+        .send({ success: true });
     });
 
     // GET
