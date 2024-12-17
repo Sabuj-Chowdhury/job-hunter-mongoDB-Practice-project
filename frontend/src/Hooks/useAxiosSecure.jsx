00e6f7dc6dba2303156ea767ce55be91dc46ea51
Sabuj-Chowdhury <csabuj73@gmail.com>
2024-12-17 23:56:52 +0530

    axios interceptor on the response in custome hook to secure

--- frontend/src/Hooks/useAxiosSecure.jsx
+++ frontend/src/Hooks/useAxiosSecure.jsx
@@ -1,4 +1,7 @@
 import axios from "axios";
+import { useEffect } from "react";
+import useAuth from "./useAuth";
+import { useNavigate } from "react-router-dom";
 
 // axios instance
 const axiosInstance = axios.create({
@@ -7,6 +10,25 @@ const axiosInstance = axios.create({
 });
 
 const useAxiosSecure = () => {
+  const { logout } = useAuth();
+  const navigate = useNavigate();
+  useEffect(() => {
+    axiosInstance.interceptors.response.use(
+      (response) => {
+        return response;
+      },
+      (error) => {
+        console.log("error in interceptor", error);
+        if (error.status === 401 || error.status === 403) {
+          // logout the user
+          logout();
+          navigate("/login");
+        }
+
+        return Promise.reject(error);
+      }
+    );
+  }, [logout, navigate]);
   return axiosInstance;
 };
 
